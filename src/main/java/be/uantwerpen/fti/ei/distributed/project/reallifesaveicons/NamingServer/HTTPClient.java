2a0db7308544877b208c1dc47b79c393c5971902
siemenHerremans <siemen_herremans@hotmail.com>
2020-05-05 16:35:58 +0200

    Update HTTPClient.java

--- src/main/java/be/uantwerpen/fti/ei/distributed/project/reallifesaveicons/NamingServer/HTTPClient.java
+++ src/main/java/be/uantwerpen/fti/ei/distributed/project/reallifesaveicons/NamingServer/HTTPClient.java
@@ -1,12 +1,17 @@
 package be.uantwerpen.fti.ei.distributed.project.reallifesaveicons.NamingServer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
+
 @Component
 public class HTTPClient {
 
+    //Logger
+    private static final Logger logger = LoggerFactory.getLogger(NamingServerServiceImpl.class);
     private final RestTemplate restTemplate;
 
     public HTTPClient(RestTemplateBuilder restTemplateBuilder) {
@@ -15,6 +20,7 @@ public class HTTPClient {
 
     public void putHTTP(String ip, String data){
         String url = "http://" + ip + ":8081/" + data;
+        logger.info(url);
         restTemplate.put(url, String.class);
     }
 }
