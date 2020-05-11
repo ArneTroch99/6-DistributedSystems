93b1b5a4190ea40fb30558895f94d763cb03f0bd
siemenHerremans <siemen_herremans@hotmail.com>
2020-05-11 21:48:31 +0200

    Added "allnodes"

--- src/main/java/be/uantwerpen/fti/ei/distributed/project/reallifesaveicons/NamingServer/HTTPClient.java
+++ src/main/java/be/uantwerpen/fti/ei/distributed/project/reallifesaveicons/NamingServer/HTTPClient.java
@@ -3,6 +3,7 @@ package be.uantwerpen.fti.ei.distributed.project.reallifesaveicons.NamingServer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
@@ -22,5 +23,10 @@ public class HTTPClient {
         String url = "http://" + ip + ":8081/" + data;
         logger.info(url);
         restTemplate.put(url, String.class);
+    }
+
+    public ResponseEntity<String> getHTTP(String ip, String data){
+        String url = "http://" + ip + ":8081/" + data;
+        return restTemplate.getForEntity(url, String.class);
     }
 }
